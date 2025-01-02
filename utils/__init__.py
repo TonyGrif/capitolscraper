2d34afda03fce80b3b6db614727a5e8ff33e2c37
TonyGrif <TonyGriffin2000@gmail.com>
2025-01-02 14:14:52 -0500

    utils(scraper): add trade stats getter

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+"""This module contains utility functions for this project"""
+
+from .scraperfuncs import make_request, parse_trade_stats
+
+__all__ = ["make_request", "parse_trade_stats"]
---
+++ utils/scraperfuncs.py
@@ -0,0 +1,27 @@
+"""This module contains utility functions for the scraper module"""
+
+from typing import Tuple
+import httpx
+from bs4 import BeautifulSoup
+
+
+def make_request() -> httpx.Response:
+    """Make a request on capitoltrades"""
+    # TODO: modularize to work with other pages
+    return httpx.get("https://www.capitoltrades.com/trades")
+
+
+def parse_trade_stats(text: str) -> Tuple[str, str, str, str, str]:
+    """Parse the page for trade stats"""
+    soup = BeautifulSoup(text, "html.parser")
+
+    elems = soup.find_all(
+        "div",
+        {"class": "text-size-5 font-medium leading-6 text-txt"},
+    )
+
+    vals = []
+    for elem in elems:
+        vals.append(elem.text)
+
+    return tuple(vals)
