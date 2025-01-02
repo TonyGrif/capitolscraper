36fe23356ae126b169a3d232275985ddb649f6a2
TonyGrif <TonyGriffin2000@gmail.com>
2025-01-02 13:27:16 -0500

    scraper(trades): create tradesmeta class for storing total trade stats

--- scraper/trades.py
+++
@@ -1,9 +0,0 @@
-"""This modules contains the capitoltrades scraper for the trades page"""
-
-
-class Trades:
-    """Class responsible for scraping the capitoltrades trade page"""
-
-    def __init__(self) -> None:
-        """Constructor for the Trades class"""
-        pass
---
+++ scraper/trades/__init__.py
@@ -0,0 +1,7 @@
+"""This modules contains scrapers for the trades page"""
+
+from .trades import Trades
+
+__all__ = [
+    "Trades",
+]
