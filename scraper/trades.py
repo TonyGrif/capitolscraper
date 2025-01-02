0e96096cea53ee221a5554051ac3b5671e389b3f
TonyGrif <TonyGriffin2000@gmail.com>
2025-01-02 11:58:34 -0500

    scraper(trades): create trades scraper class, add scraper directory to hatchling build

---
+++ scraper/trades.py
@@ -0,0 +1,9 @@
+"""This modules contains the capitoltrades scraper for the trades page"""
+
+
+class Trades:
+    """Class responsible for scraping the capitoltrades trade page"""
+
+    def __init__(self) -> None:
+        """Constructor for the Trades class"""
+        pass
