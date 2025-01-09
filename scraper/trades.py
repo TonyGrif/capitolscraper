b7e975ecc257de649f19df53136db7d1553b560b
TonyGrif <TonyGriffin2000@gmail.com>
2025-01-09 13:56:12 -0500

    scraper(trades): update scraper functions to use properties and set up total page counter

--- scraper/trades.py
+++ scraper/trades.py
@@ -2,7 +2,7 @@
 
 from typing import Optional
 
-from utils import make_request, parse_trade_stats
+from utils import make_request, parse_page_data, parse_trade_stats
 from utils.dataclasses import TradesStats
 
 
@@ -12,7 +12,9 @@ class Trades:
     def __init__(self) -> None:
         """Constructor for the Trades class"""
         self._stats: Optional[TradesStats] = None
+        self._total_pages: Optional[int] = None
 
+    @property
     def stats(self) -> TradesStats:
         """Return the total stats of trades"""
         if self._stats is not None:
@@ -21,3 +23,13 @@ class Trades:
         res = make_request("trades")
         self._stats = parse_trade_stats(res.text)
         return self._stats
+
+    @property
+    def total_pages(self) -> int:
+        """Return the total number of pages to scrape"""
+        if self._total_pages is not None:
+            return self._total_pages
+
+        res = make_request("trades")
+        self._total_pages = parse_page_data(res.text).total
+        return self._total_pages
