b80775ba5862eb5e5c4b07ad612dba128131c98d
TonyGrif <TonyGriffin2000@gmail.com>
2025-01-09 13:56:12 -0500

    scraper(trades): update scraper functions to use properties and set up total page counter

--- tests/scraper/test_trades.py
+++ tests/scraper/test_trades.py
@@ -3,17 +3,30 @@ import pytest
 from scraper import Trades
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def trades():
     return Trades()
 
 
 class TestTrades:
-    def test_meta_stats(self, trades):
-        meta = trades.stats()
+    def test_stats(self, trades):
+        first_stats = trades.stats
+        assert first_stats.trades is not None
+        assert first_stats.filings is not None
+        assert first_stats.volume is not None
+        assert first_stats.politicians is not None
+        assert first_stats.issuers is not None
 
-        assert meta.trades is not None
-        assert meta.filings is not None
-        assert meta.volume is not None
-        assert meta.politicians is not None
-        assert meta.issuers is not None
+        second_stats = trades.stats
+        assert second_stats.trades == first_stats.trades
+        assert second_stats.filings == first_stats.filings
+        assert second_stats.volume == first_stats.volume
+        assert second_stats.politicians == first_stats.politicians
+        assert second_stats.issuers == first_stats.issuers
+
+    def test_total_pages(self, trades):
+        first = trades.total_pages
+        assert first is not None
+
+        second = trades.total_pages
+        assert second == first
